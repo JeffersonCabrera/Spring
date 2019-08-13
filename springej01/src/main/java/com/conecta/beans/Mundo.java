eb4886a601757e632ac4e82e35eccff49b4b704c
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2019-08-13 11:08:35 -0500

    Anotaciones

--- springej01/src/main/java/com/conecta/beans/Mundo.java
+++ springej01/src/main/java/com/conecta/beans/Mundo.java
@@ -1,7 +1,10 @@
 package com.conecta.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Mundo {
 
+	@Value("Hola Conecta")
 	private String saludo;
 
 	public String getSaludo() {
--- springej01/src/main/java/com/conecta/springej01/App.java
+++ springej01/src/main/java/com/conecta/springej01/App.java
@@ -1,17 +1,32 @@
 package com.conecta.springej01;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.conecta.beans.Mundo; 
+import com.conecta.beans.AppConfig;
+import com.conecta.beans.AppConfig2;
+import com.conecta.beans.Mundo;
 
 public class App {
 
-	public static void main(String[] args) { 
-		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/conecta/xml/beans.xml");
-		Mundo m = (Mundo) appContext.getBean("mundo");
+	public static void main(String[] args) {
+
+//		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/conecta/xml/beans.xml");
+//		Mundo m = (Mundo) appContext.getBean("mundo");
+//		System.out.println(m.getSaludo()); 
+//		((ConfigurableApplicationContext)appContext).close();
+
+//		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
+		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+		appContext.register(AppConfig.class);
+		appContext.register(AppConfig2.class);
+		appContext.refresh();
+		Mundo m = (Mundo) appContext.getBean("marte");
 		System.out.println(m.getSaludo());
+		((ConfigurableApplicationContext) appContext).close();
+
 	}
 
 }
