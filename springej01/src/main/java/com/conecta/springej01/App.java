7e770ffceb733474c858d3ef9d1a468426bfddc7
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2019-08-13 12:14:21 -0500

    Inyección Constructor

--- springej01/src/main/java/com/conecta/springej01/App.java
+++ springej01/src/main/java/com/conecta/springej01/App.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.conecta.beans.AppConfig;
 import com.conecta.beans.AppConfig2;
 import com.conecta.beans.Mundo;
+import com.conecta.beans.Persona;
 
 public class App {
 
@@ -19,12 +20,19 @@ public class App {
 //		((ConfigurableApplicationContext)appContext).close();
 
 //		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
-		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
-		appContext.register(AppConfig.class);
-		appContext.register(AppConfig2.class);
-		appContext.refresh();
-		Mundo m = (Mundo) appContext.getBean("marte");
-		System.out.println(m.getSaludo());
+//		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+//		appContext.register(AppConfig.class);
+//		appContext.register(AppConfig2.class);
+//		appContext.refresh();
+//		Mundo m = (Mundo) appContext.getBean("marte");
+//		System.out.println(m.getSaludo());
+//		((ConfigurableApplicationContext) appContext).close();
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/conecta/xml/beans.xml");
+		Persona per = (Persona) appContext.getBean("persona");
+		
+		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo());
+		
 		((ConfigurableApplicationContext) appContext).close();
 
 	}
