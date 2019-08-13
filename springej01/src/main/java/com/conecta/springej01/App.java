ffacd22edb537d501c7d7deb333beb044d9b7752
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2019-08-13 18:32:58 -0500

    BeanScopes

--- springej01/src/main/java/com/conecta/springej01/App.java
+++ springej01/src/main/java/com/conecta/springej01/App.java
@@ -31,7 +31,11 @@ public class App {
 
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/conecta/xml/beans.xml");
 		Persona per = (Persona) appContext.getBean("persona");
+		Persona per2 = (Persona) appContext.getBean("persona");
 		
+		per.setId(1);
+		per.setNombre("Jefferson");
+		per.setApodo("Jeff");
 //		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " "+ per.getPais().getNombre());
 		
 //		String nombreCuidades = "";
@@ -42,6 +46,10 @@ public class App {
 //		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " "+ per.getPais().getNombre() + " " + nombreCuidades);
 		
 		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " "+ per.getPais().getNombre() + " " + per.getCiudad().getNombre());
+		System.out.println(per2.getId() + " " + per2.getNombre() + " " + per2.getApodo() + " "+ per2.getPais().getNombre() + " " + per2.getCiudad().getNombre());
+		System.out.println(per);
+		System.out.println(per2);
+				
 		((ConfigurableApplicationContext) appContext).close();
 
 	}
