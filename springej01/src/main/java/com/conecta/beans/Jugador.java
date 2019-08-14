6baa88624f0b1811051880d823723975e1a1c0b9
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2019-08-14 14:43:45 -0500

    Anotación Autowired y Qualifier

--- springej01/src/main/java/com/conecta/beans/Jugador.java
+++ springej01/src/main/java/com/conecta/beans/Jugador.java
@@ -1,5 +1,7 @@
 package com.conecta.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 
 import com.conecta.interfaces.IEquipo;
@@ -8,7 +10,8 @@ public class Jugador {
 
 	private int numero;
 	private String nombre;
-	
+	@Autowired
+	@Qualifier("barcelonaQualifier")
 	private IEquipo equipo;
 
 	public int getNumero() {
@@ -32,7 +35,7 @@ public class Jugador {
 		return equipo;
 	}
 
-	@Required
+//	@Required
 	public void setEquipo(IEquipo equipo) {
 		this.equipo = equipo;
 	}
