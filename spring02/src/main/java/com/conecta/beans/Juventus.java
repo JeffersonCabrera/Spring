15c27578438e6ee5d2643cf845a6f324dd416536
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2019-08-14 16:35:19 -0500

    Ejercicio 1 XML

---
+++ spring02/src/main/java/com/conecta/beans/Juventus.java
@@ -0,0 +1,12 @@
+package com.conecta.beans;
+
+import com.conecta.interfaces.IEquipo;
+
+public class Juventus implements IEquipo {
+
+	public String mostrar() {
+		// TODO Auto-generated method stub
+		return "Juventus";
+	}
+
+}
