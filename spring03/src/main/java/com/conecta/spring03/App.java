1d65f6fe8b9617cb2f43e86557683803ca63e979
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2019-08-15 07:49:16 -0500

    Ejercicio2 Anotarionces 2

--- spring03/src/main/java/com/conecta/spring03/App.java
+++ spring03/src/main/java/com/conecta/spring03/App.java
@@ -1,12 +1,45 @@
 package com.conecta.spring03;
 
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.conecta.beans.Barcelona;
+import com.conecta.beans.Jugador;
+import com.conecta.beans.Juventus;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    { 
-    }
+public class App {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Elija un equipo: 1-Barcelona 2-Juventus");
+
+		int respuesta = sc.nextInt();
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/conecta/xml/beans.xml");
+		Jugador ju = (Jugador) appContext.getBean("jugador1");
+
+		switch (respuesta) {
+		case 1:
+			ju.setEquipo(new Barcelona());
+			break;
+		case 2:
+			ju.setEquipo(new Juventus());
+			break;
+
+		default:
+			break;
+		}
+
+		System.out.println(ju.getNombre() + " - " + ju.getEquipo().mostrar() + " - " + ju.getCamiseta().getNumero()
+				+ " - " + ju.getCamiseta().getMarca().getNombre());
+
+		((ConfigurableApplicationContext) appContext).close();
+	}
 }
